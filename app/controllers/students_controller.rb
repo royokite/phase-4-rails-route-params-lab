13f44f6b434d1eada5634cd312ff649ecaac84fe
RoyOkite <royokite@gmail.com>
2022-11-24 19:41:39 +0300

    Code along

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -2,7 +2,12 @@ class StudentsController < ApplicationController
 
   def index
     students = Student.all
-    render json: students
+    render json: students, except: [:created_at, :updated_at]
+  end
+
+  def show
+    student = Student.find(params[:id])
+    render json: student, except: [:created_at, :updated_at]
   end
 
 end
